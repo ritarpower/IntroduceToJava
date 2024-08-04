27a85106b13164d1e7af84c6f80e1f5b6f9e7f47
Minh Phuc <caoquangminhphuc@gmail.com>
2024-08-04 15:38:54 +0700

    add file bai tap

---
+++ HelloName/src/HelloName.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HelloName {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao ten cua ban:");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
---
+++ TranferMoney/src/TranferMoney.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TranferMoney {
+    public static void main(String[] args) {
+        int rate = 23000;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao so tien can quy doi(USD):");
+        int usd = Integer.parseInt(sc.nextLine());
+        System.out.println("Quy thanh VND la: " + rate * usd);
+    }
+}
